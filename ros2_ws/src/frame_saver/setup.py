331f704002d1e65113f49f51cf425ccbbf11d8e1
Triet Nguyen <tr_nguye@insa-toulouse.fr>
2026-03-05 11:32:33 +0100

    bump frame_saver to version 1.0

--- ros2_ws/src/frame_saver/setup.py
+++ ros2_ws/src/frame_saver/setup.py
@@ -6,7 +6,7 @@ package_name = 'frame_saver'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
